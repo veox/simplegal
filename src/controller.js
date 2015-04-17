4be6e3e7025a46304203d2c88dff563de7140ad6
Noel Maersk <veox@wemakethings.net>
2015-04-17 17:29:17 +0300

    Minimal working version.
    Doesn't handle bad input or anything. CSS is lame.

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,17 @@
 var simplegalApp = angular.module('simplegalApp', []);
 
 simplegalApp.controller('galCtrl', function($scope) {
-    $scope.date = '2015-04-17';
+    $scope.date = '2014-12-31';
+    $scope.image = getImagePath($scope.date);
     
-    $scope.getDate = function() {
-	return $scope.date;
+    function getImagePath(date) {
+	date = date.split("-"); // ['year', 'month', 'day']
+	var image = 'img' + '/' + date[0] + '/' + date[1] + '/' +
+	    + date[0] + date[1] + date[2] + '.jpeg';
+	return image;
     };
+
+    $scope.updateImage = function() {
+	$scope.image = getImagePath($scope.date);
+    }
 });
