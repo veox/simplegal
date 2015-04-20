e5e351f1e83574e0dc6d8e3d3be3a5d7f9bedb53
Noel Maersk <veox@wemakethings.net>
2015-04-20 20:34:38 +0300

    Allow several images per day (hackish).

--- src/simplegal.js
+++ src/simplegal.js
@@ -15,18 +15,29 @@ simplegalApp.directive('errSrc', function() {
 simplegalApp.controller('galCtrl', ['$scope', function($scope) {
     // double date hack to disregard timezone
     $scope.date = new Date(Date.UTC(2014, 11, 31));
-    $scope.image = getImagePath($scope.date);
+    $scope.image = getImagePath($scope.date, 0);
+    $scope.image1 = getImagePath($scope.date, 1);
+    $scope.image2 = getImagePath($scope.date, 2);
+    $scope.image3 = getImagePath($scope.date, 3);
+    $scope.image4 = getImagePath($scope.date, 4);
     $scope.image404 = 'img/404.jpeg';
+    $scope.blank = 'img/blank.png';
 
-    function getImagePath(date) {
+    function getImagePath(date, nth) {
 	date = date.toISOString();
 	date = date.split("T")[0];
 	date = date.split("-");
 	var y = date[0];
 	var m = date[1];
 	var d = date[2];
+
+	var n = '';
+	if (nth > 0) {
+	    n = '-' + nth;
+	}
+	
 	var image = 'img' + '/' + y + '/' + m + '/' +
-	    + y + m + d + '.jpeg';
+	    + y + m + d + n + '.jpeg';
 	return image;
     };
 
